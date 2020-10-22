6adcd8dc92bbc234c9fee434088420545cba30d8
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2020-10-21 23:38:09 -0500

    Added audio support
    Added support for videos with audio, along with some variable optimizations and grammar corrections

--- run CAIN.bat
+++ run CAIN.bat
@@ -1,24 +1,28 @@
 @echo off
 
+timeout 2 >nul
 echo NOTES: This thing doesn't supports audio exporting since I'm to lazy to do it on the first version
-timeout 2 >null
+timeout 3 >nul
 echo It's not perfect but it does the job
-timeout 2 >null
+timeout 2 >nul
 
 rem Frame extraction
 
+echo.
 set /p videopath="Enter your video directory: "
-echo Initialazing frame extraction...
-timeout 1 >nul
+set /p audio="Does video has audio?(y/n): "
+echo.
+echo Initializing frame extraction...
 md Original_frames >nul
 ffmpeg -loglevel quiet -i %videopath% "%cd%\Original_frames\%%06d.png"
 echo Frame extraction completed...
+if %audio%==y (ffmpeg -loglevel quiet -i %videopath% "%cd%\Audio.wav")
 timeout 2 >nul
 
 rem Frame Interpolation
 
+echo.
 echo Starting frame interpolation...
-timeout 1 >nul
 md Interpolated_frames >nul
 cain-ncnn-vulkan -i "%cd%\Original_frames" -o "%cd%\Interpolated_frames" -t 2048 -g 0
 echo Frame interpolation completed...
@@ -26,23 +30,26 @@ timeout 2 >nul
 
 rem Frame to video
 
+echo.
 set /p ftv="Want to convert the frames to video (y/n): "
-if %ftv%==y (set /p framerate="Please specify the framerate: " & set /p crf="Please specify a CRF value: " & echo Generating video, please wait & set ffmpegstart=y)
-if %ffmpegstart%==y (ffmpeg -loglevel quiet -framerate %framerate% -i "%cd%\Interpolated_frames\%%6d.png" -c:v libx264 -preset veryslow -crf %crf% "%cd%\FinalVideo.mp4")
-if %ftv%==n (echo Ok)
-echo Done
+if %ftv%==y (set /p framerate="Please specify the framerate: " & set /p crf="Please specify a CRF value: " & echo Generating video, please wait) else (echo Ok... & set audio=null)
+if %audio%==y (ffmpeg.exe -loglevel quiet -framerate %framerate% -i "%cd%\Interpolated_frames\%%6d.png" -i "%cd%\Audio.wav" -c:v libx264 -preset veryslow -crf %crf% -c:a aac "%cd%\FinalVideo.mp4" & echo Video finished, check out the folder for the result)
+if %audio%==n (ffmpeg.exe -loglevel quiet -framerate %framerate% -i "%cd%\Interpolated_frames\%%6d.png" -c:v libx264 -preset veryslow -crf %crf% "%cd%\FinalVideo.mp4" & echo Video finished, check out the folder for the result)
 timeout 2 >nul
 
 rem Delete cache
 
+echo.
 set /p cache="Want to delete the cache (y/n): "
-If %cache%==y (echo OK, deleting cache & rd /s /q "%cd%\Interpolated_frames" & rd /s /q "%cd%\Original_frames" & echo Cache deleted) else (echo Ok...)
-timeout 1 >nul
+If %cache%==y (echo OK, deleting cache & rd /s /q "%cd%\Interpolated_frames" >nul & rd /s /q "%cd%\Original_frames" >nul & del /q "%cd%\Audio.wav" >nul & timeout 1 >nul & echo Cache deleted) else (echo Ok...)
+timeout 2 >nul
 
 rem end
 
-echo "Thanks for wasting my time"
-echo "Script written by Katzenwerfer"
-timeout 3 >nul
-del /q "%cd%\null"
+echo.
+echo Thanks for wasting my time
+timeout 1 >nul
+echo.
+echo Script written by Katzenwerfer
+timeout 2 >nul
 pause
