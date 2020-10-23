18846b1e426fa8d006497b76e8eaa252cd91e8f9
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2020-10-23 09:47:40 -0500

    Added GIF support

--- run CAIN.bat
+++ run CAIN.bat
@@ -1,8 +1,11 @@
 @echo off
-
 timeout 2 >nul
-echo Changelog: added support for audio exporting
-timeout 3 >nul
+echo Version 3
+timeout 2 >nul
+echo Changelog: added support for gif (No alpha channel)
+echo            Moved some commands here and there
+timeout 2 >nul
+echo.
 echo It's not perfect but it does the job
 timeout 2 >nul
 
@@ -10,13 +13,11 @@ rem Frame extraction
 
 echo.
 set /p videopath="Enter your video directory: "
-set /p audio="Does video has audio?(y/n): "
 echo.
 echo Initializing frame extraction...
 md Original_frames >nul
 ffmpeg -loglevel quiet -i %videopath% "%cd%\Original_frames\%%06d.png"
 echo Frame extraction completed...
-if %audio%==y (ffmpeg -loglevel quiet -i %videopath% "%cd%\Audio.wav")
 timeout 2 >nul
 
 rem Frame Interpolation
@@ -32,16 +33,21 @@ rem Frame to video
 
 echo.
 set /p ftv="Want to convert the frames to video (y/n): "
+if %ftv%==y (set /p audio="Does input has audio?(y/n): ")
+if %ftv%==y (set /p gif="Want gif instead of video?(y/n): " & set audio=null & set ftv=n)
+if %audio%==y (ffmpeg -loglevel quiet -i %videopath% "%cd%\Audio.wav")
+if %gif%==y (ffmpeg -loglevel quiet -i %videopath% -vf palettegen "%cd%\palette.png")
 if %ftv%==y (set /p framerate="Please specify the framerate: " & set /p crf="Please specify a CRF value: " & echo Generating video, please wait) else (echo Ok... & set audio=null)
+if %gif%==y (set /p framerate="Please specify the framerate: " & echo Generating gif, please wait)
 if %audio%==y (ffmpeg.exe -loglevel quiet -framerate %framerate% -i "%cd%\Interpolated_frames\%%6d.png" -i "%cd%\Audio.wav" -c:v libx264 -preset veryslow -crf %crf% -c:a aac "%cd%\FinalVideo.mp4" & echo Video finished, check out the folder for the result)
 if %audio%==n (ffmpeg.exe -loglevel quiet -framerate %framerate% -i "%cd%\Interpolated_frames\%%6d.png" -c:v libx264 -preset veryslow -crf %crf% "%cd%\FinalVideo.mp4" & echo Video finished, check out the folder for the result)
+if %gif%==y (ffmpeg.exe -loglevel quiet -framerate %framerate% -i "%cd%\Interpolated_frames\%%6d.png" -i "%cd%\palette.png" -filter_complex "[0:v][1:v] paletteuse" "%cd%\FinalGIF.gif")
 timeout 2 >nul
-
 rem Delete cache
 
 echo.
 set /p cache="Want to delete the cache (y/n): "
-If %cache%==y (echo OK, deleting cache & rd /s /q "%cd%\Interpolated_frames" >nul & rd /s /q "%cd%\Original_frames" >nul & del /q "%cd%\Audio.wav" >nul & timeout 1 >nul & echo Cache deleted) else (echo Ok...)
+If %cache%==y (echo OK, deleting cache & rd /s /q "%cd%\Interpolated_frames" >nul & rd /s /q "%cd%\Original_frames" >nul & del /q "%cd%\Audio.wav" >nul & del /q "%cd%\palette.png" >nul & timeout 1 >nul & echo Cache deleted) else (echo Ok...)
 timeout 2 >nul
 
 rem end
