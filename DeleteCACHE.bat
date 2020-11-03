ab564bb1e9ba9046f6124070b830c2b00e716065
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2020-11-02 22:34:01 -0600

    Update DeleteCACHE.bat

--- DeleteCACHE.bat
+++ DeleteCACHE.bat
@@ -1,13 +1,15 @@
 @echo off
 
+echo This deletes all cache made by any of the CAIN batchs
+timeout 1 >nul
 set /p cache="Are you sure you want to delete the cache (y/n): "
-If %cache%==y (echo OK, deleting cache & rd /s /q "%cd%\Interpolated_frames" >nul & rd /s /q "%cd%\Original_frames" >nul & del /q "%cd%\Audio.wav" >nul & del /q "%cd%\palette.png" >nul & timeout 1 >nul & echo Cache deleted) else (echo Ok...)
-timeout 2 >nul
+If %cache%==y (echo OK, deleting cache & rd /s /q "%cd%\Original_frames" >nul & rd /s /q "%cd%\Interpolated_frames_2x" >nul & rd /s /q "%cd%\Interpolated_frames_4x" >nul & rd /s /q "%cd%\Interpolated_frames_8x" >nul & rd /s /q "%cd%\Interpolated_frames_8x" >nul & rd /s /q "%cd%\Interpolated_frames_16x" >nul & del /q "%cd%\Audio.wav" >nul & del /q "%cd%\palette.png" >nul & del /q "%cd%\rate" >nul & timeout 1 >nul & echo Cache deleted) else (echo Ok...)
+timeout 1 >nul
 
 echo.
 echo Thanks for wasting my time
 timeout 1 >nul
 echo.
 echo Script written by Katzenwerfer
-timeout 2 >nul
+timeout 1 >nul
 pause
